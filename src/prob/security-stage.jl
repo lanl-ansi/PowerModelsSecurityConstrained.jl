d1c47e37dcf56a353c0018335f846be7b9687fce
ccoffrin <carleton@coffrin.com>
2020-03-14 15:52:07 -0600

    refactor security-stage models

--- src/prob/security-stage.jl
+++ src/prob/security-stage.jl
@@ -309,54 +309,12 @@ function build_pf_soft_rect(pm::AbstractPowerModel)
 
 
     start_time = time()
-    p = Dict{Tuple{Int64,Int64,Int64},GenericQuadExpr{Float64,VariableRef}}()
-    q = Dict{Tuple{Int64,Int64,Int64},GenericQuadExpr{Float64,VariableRef}}()
-    for (i,branch) in ref(pm, :branch)
-        #PowerModels.constraint_ohms_yt_from(pm, i)
-        #PowerModels.constraint_ohms_yt_to(pm, i)
-
-        f_bus_id = branch["f_bus"]
-        t_bus_id = branch["t_bus"]
-        f_idx = (i, f_bus_id, t_bus_id)
-        t_idx = (i, t_bus_id, f_bus_id)
-
-        f_bus = ref(pm, :bus, f_bus_id)
-        t_bus = ref(pm, :bus, t_bus_id)
-
-        #g, b = PowerModels.calc_branch_y(branch)
-        #tr, ti = PowerModels.calc_branch_t(branch)
-        g = branch["g"]
-        b = branch["b"]
-        tr = branch["tr"]
-        ti = branch["ti"]
-
-        g_fr = branch["g_fr"]
-        b_fr = branch["b_fr"]
-        g_to = branch["g_to"]
-        b_to = branch["b_to"]
-        tm = branch["tap"]
-
-        #p_fr  = var(pm, :p, f_idx)
-        #q_fr  = var(pm, :q, f_idx)
-        #p_to  = var(pm, :p, t_idx)
-        #q_to  = var(pm, :q, t_idx)
-        vr_fr = vr[f_bus_id] #var(pm, :vr, f_bus_id)
-        vr_to = vr[t_bus_id] #var(pm, :vr, t_bus_id)
-        vi_fr = vi[f_bus_id] #var(pm, :vi, f_bus_id)
-        vi_to = vi[t_bus_id] #var(pm, :vi, t_bus_id)
-
-
-        if branch["transformer"]
-            p[f_idx] = (g/tm^2+g_fr)*(vr_fr^2 + vi_fr^2) + (-g*tr+b*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-b*tr-g*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-            q[f_idx] = -(b/tm^2+b_fr)*(vr_fr^2 + vi_fr^2) - (-b*tr-g*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-g*tr+b*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-        else
-            p[f_idx] = (g+g_fr)/tm^2*(vr_fr^2 + vi_fr^2) + (-g*tr+b*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-b*tr-g*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-            q[f_idx] = -(b+b_fr)/tm^2*(vr_fr^2 + vi_fr^2) - (-b*tr-g*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-g*tr+b*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-        end
-        p[t_idx] = (g+g_to)*(vr_to^2 + vi_to^2) + (-g*tr-b*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-b*tr+g*ti)/tm^2*(-(vi_fr*vr_to - vr_fr*vi_to))
-        q[t_idx] = -(b+b_to)*(vr_to^2 + vi_to^2) - (-b*tr+g*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-g*tr-b*ti)/tm^2*(-(vi_fr*vr_to - vr_fr*vi_to))
-
+    for i in ids(pm, :branch)
+        expression_branch_power_yt_from_goc(pm, i)
+        expression_branch_power_yt_to(pm, i)
     end
+    p = var(pm, :p)
+    q = var(pm, :q)
     #Memento.info(LOGGER, "flow expr time: $(time() - start_time)")
 
 
@@ -399,7 +357,6 @@ function build_pf_soft_rect(pm::AbstractPowerModel)
         #PowerModels.constraint_power_balance(pm, i)
 
         bus_arcs = ref(pm, :bus_arcs, i)
-        bus_arcs_dc = ref(pm, :bus_arcs_dc, i)
         bus_gens = ref(pm, :bus_gens, i)
         bus_loads = ref(pm, :bus_loads, i)
         bus_shunts = ref(pm, :bus_shunts, i)
@@ -749,55 +706,10 @@ function build_fixed_pf_nbf_rect2(pm::AbstractPowerModel)
     end
     #Memento.info(LOGGER, "misc constraints time: $(time() - start_time)")
 
-
     start_time = time()
-    p = Dict{Tuple{Int64,Int64,Int64},GenericQuadExpr{Float64,VariableRef}}()
-    q = Dict{Tuple{Int64,Int64,Int64},GenericQuadExpr{Float64,VariableRef}}()
-    for (i,branch) in ref(pm, :branch)
-        #PowerModels.constraint_ohms_yt_from(pm, i)
-        #PowerModels.constraint_ohms_yt_to(pm, i)
-
-        f_bus_id = branch["f_bus"]
-        t_bus_id = branch["t_bus"]
-        f_idx = (i, f_bus_id, t_bus_id)
-        t_idx = (i, t_bus_id, f_bus_id)
-
-        f_bus = ref(pm, :bus, f_bus_id)
-        t_bus = ref(pm, :bus, t_bus_id)
-
-        #g, b = PowerModels.calc_branch_y(branch)
-        #tr, ti = PowerModels.calc_branch_t(branch)
-        g = branch["g"]
-        b = branch["b"]
-        tr = branch["tr"]
-        ti = branch["ti"]
-
-        g_fr = branch["g_fr"]
-        b_fr = branch["b_fr"]
-        g_to = branch["g_to"]
-        b_to = branch["b_to"]
-        tm = branch["tap"]
-
-        #p_fr  = var(pm, :p, f_idx)
-        #q_fr  = var(pm, :q, f_idx)
-        #p_to  = var(pm, :p, t_idx)
-        #q_to  = var(pm, :q, t_idx)
-        vr_fr = vr[f_bus_id] #var(pm, :vr, f_bus_id)
-        vr_to = vr[t_bus_id] #var(pm, :vr, t_bus_id)
-        vi_fr = vi[f_bus_id] #var(pm, :vi, f_bus_id)
-        vi_to = vi[t_bus_id] #var(pm, :vi, t_bus_id)
-
-
-        if branch["transformer"]
-            p[f_idx] = (g/tm^2+g_fr)*(vr_fr^2 + vi_fr^2) + (-g*tr+b*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-b*tr-g*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-            q[f_idx] = -(b/tm^2+b_fr)*(vr_fr^2 + vi_fr^2) - (-b*tr-g*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-g*tr+b*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-        else
-            p[f_idx] = (g+g_fr)/tm^2*(vr_fr^2 + vi_fr^2) + (-g*tr+b*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-b*tr-g*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-            q[f_idx] = -(b+b_fr)/tm^2*(vr_fr^2 + vi_fr^2) - (-b*tr-g*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-g*tr+b*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-        end
-        p[t_idx] = (g+g_to)*(vr_to^2 + vi_to^2) + (-g*tr-b*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-b*tr+g*ti)/tm^2*(-(vi_fr*vr_to - vr_fr*vi_to))
-        q[t_idx] = -(b+b_to)*(vr_to^2 + vi_to^2) - (-b*tr+g*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-g*tr-b*ti)/tm^2*(-(vi_fr*vr_to - vr_fr*vi_to))
-
+    for i in ids(pm, :branch)
+        expression_branch_power_yt_from_goc(pm, i)
+        expression_branch_power_yt_to(pm, i)
     end
     #Memento.info(LOGGER, "flow expr time: $(time() - start_time)")
 
@@ -807,51 +719,33 @@ function build_fixed_pf_nbf_rect2(pm::AbstractPowerModel)
     qg = Dict{Int,Any}()
     for (i,gen) in ref(pm, :gen)
         if gen["pg_fixed"]
-            pg[i] = gen["pg"]
             @constraint(pm.model, var(pm, :pg, i) == gen["pg"])
+            pg[i] = gen["pg"]
         else
             if i in ref(pm, :response_gens)
-                pg[i] = gen["pg_base"] + gen["alpha"]*delta
                 @constraint(pm.model, var(pm, :pg, i) == gen["pg_base"] + gen["alpha"]*delta)
+                pg[i] = gen["pg_base"] + gen["alpha"]*delta
             else
-                pg[i] = gen["pg_base"]
                 @constraint(pm.model, var(pm, :pg, i) == gen["pg_base"])
+                pg[i] = gen["pg_base"]
             end
         end
 
         if gen["qg_fixed"]
-            qg[i] = gen["qg"]
             @constraint(pm.model, var(pm, :qg, i) == gen["qg"])
+            qg[i] = gen["qg"]
         else
             qg[i] = var(pm, :qg, i)
         end
     end
+    var(pm, pm.cnw)[:pg] = pg
+    var(pm, pm.cnw)[:qg] = qg
     #Memento.info(LOGGER, "gen expr time: $(time() - start_time)")
 
 
     start_time = time()
     for (i,bus) in ref(pm, :bus)
-        #PowerModels.constraint_power_balance(pm, i)
-
-        bus_arcs = ref(pm, :bus_arcs, i)
-        bus_arcs_dc = ref(pm, :bus_arcs_dc, i)
-        bus_gens = ref(pm, :bus_gens, i)
-        bus_loads = ref(pm, :bus_loads, i)
-        bus_shunts = ref(pm, :bus_shunts, i)
-
-        bus_pd = Dict(k => ref(pm, :load, k, "pd") for k in bus_loads)
-        bus_qd = Dict(k => ref(pm, :load, k, "qd") for k in bus_loads)
-
-        bus_gs = Dict(k => ref(pm, :shunt, k, "gs") for k in bus_shunts)
-        bus_bs = Dict(k => ref(pm, :shunt, k, "bs") for k in bus_shunts)
-
-        #p = var(pm, :p)
-        #q = var(pm, :q)
-        #pg = var(pm, :pg)
-        #qg = var(pm, :qg)
-
-        @constraint(pm.model, sum(p[a] for a in bus_arcs) == sum(pg[g] for g in bus_gens) - sum(pd for pd in values(bus_pd)) - sum(gs for gs in values(bus_gs))*(vr[i]^2 + vi[i]^2))
-        @constraint(pm.model, sum(q[a] for a in bus_arcs) == sum(qg[g] for g in bus_gens) - sum(qd for qd in values(bus_qd)) + sum(bs for bs in values(bus_bs))*(vr[i]^2 + vi[i]^2))
+        PowerModels.constraint_power_balance(pm, i)
     end
     #Memento.info(LOGGER, "power balance constraint time: $(time() - start_time)")
 end
@@ -906,54 +800,12 @@ function build_fixed_pf_nbf_rect2_ds(pm::AbstractPowerModel)
 
 
     start_time = time()
-    p = Dict{Tuple{Int64,Int64,Int64},GenericQuadExpr{Float64,VariableRef}}()
-    q = Dict{Tuple{Int64,Int64,Int64},GenericQuadExpr{Float64,VariableRef}}()
-    for (i,branch) in ref(pm, :branch)
-        #PowerModels.constraint_ohms_yt_from(pm, i)
-        #PowerModels.constraint_ohms_yt_to(pm, i)
-
-        f_bus_id = branch["f_bus"]
-        t_bus_id = branch["t_bus"]
-        f_idx = (i, f_bus_id, t_bus_id)
-        t_idx = (i, t_bus_id, f_bus_id)
-
-        f_bus = ref(pm, :bus, f_bus_id)
-        t_bus = ref(pm, :bus, t_bus_id)
-
-        #g, b = PowerModels.calc_branch_y(branch)
-        #tr, ti = PowerModels.calc_branch_t(branch)
-        g = branch["g"]
-        b = branch["b"]
-        tr = branch["tr"]
-        ti = branch["ti"]
-
-        g_fr = branch["g_fr"]
-        b_fr = branch["b_fr"]
-        g_to = branch["g_to"]
-        b_to = branch["b_to"]
-        tm = branch["tap"]
-
-        #p_fr  = var(pm, :p, f_idx)
-        #q_fr  = var(pm, :q, f_idx)
-        #p_to  = var(pm, :p, t_idx)
-        #q_to  = var(pm, :q, t_idx)
-        vr_fr = vr[f_bus_id] #var(pm, :vr, f_bus_id)
-        vr_to = vr[t_bus_id] #var(pm, :vr, t_bus_id)
-        vi_fr = vi[f_bus_id] #var(pm, :vi, f_bus_id)
-        vi_to = vi[t_bus_id] #var(pm, :vi, t_bus_id)
-
-
-        if branch["transformer"]
-            p[f_idx] = (g/tm^2+g_fr)*(vr_fr^2 + vi_fr^2) + (-g*tr+b*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-b*tr-g*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-            q[f_idx] = -(b/tm^2+b_fr)*(vr_fr^2 + vi_fr^2) - (-b*tr-g*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-g*tr+b*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-        else
-            p[f_idx] = (g+g_fr)/tm^2*(vr_fr^2 + vi_fr^2) + (-g*tr+b*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-b*tr-g*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-            q[f_idx] = -(b+b_fr)/tm^2*(vr_fr^2 + vi_fr^2) - (-b*tr-g*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-g*tr+b*ti)/tm^2*(vi_fr*vr_to - vr_fr*vi_to)
-        end
-        p[t_idx] = (g+g_to)*(vr_to^2 + vi_to^2) + (-g*tr-b*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-b*tr+g*ti)/tm^2*(-(vi_fr*vr_to - vr_fr*vi_to))
-        q[t_idx] = -(b+b_to)*(vr_to^2 + vi_to^2) - (-b*tr+g*ti)/tm^2*(vr_fr*vr_to + vi_fr*vi_to) + (-g*tr-b*ti)/tm^2*(-(vi_fr*vr_to - vr_fr*vi_to))
-
+    for i in ids(pm, :branch)
+        expression_branch_power_yt_from_goc(pm, i)
+        expression_branch_power_yt_to(pm, i)
     end
+    p = var(pm, :p)
+    q = var(pm, :q)
     #Memento.info(LOGGER, "flow expr time: $(time() - start_time)")
 
 
@@ -989,7 +841,6 @@ function build_fixed_pf_nbf_rect2_ds(pm::AbstractPowerModel)
         #PowerModels.constraint_power_balance(pm, i)
 
         bus_arcs = ref(pm, :bus_arcs, i)
-        bus_arcs_dc = ref(pm, :bus_arcs_dc, i)
         bus_gens = ref(pm, :bus_gens, i)
         bus_loads = ref(pm, :bus_loads, i)
         bus_shunts = ref(pm, :bus_shunts, i)
@@ -1265,116 +1116,60 @@ function build_contingency_opf4(pm::AbstractPowerModel)
 
     PowerModels.constraint_model_voltage(pm)
 
-    vm = Dict()
+    vm = Dict{Int,Any}()
     for (i,bus) in ref(pm, :bus)
         if bus["vm_fixed"]
-            vm[i] = bus["vm_base"]
-            @constraint(pm.model, vm[i] == bus["vm_base"])
+            @constraint(pm.model, var(pm, :vm, i) == bus["vm_base"])
             @constraint(pm.model, vm_vio[i] == 0)
+            vm[i] = bus["vm_base"]
         else
+            @constraint(pm.model,  vm_vio[i] >=  var(pm, :vm, i) - bus["vmax"])
+            @constraint(pm.model,  vm_vio[i] >= -var(pm, :vm, i) + bus["vmin"])
             vm[i] = var(pm, :vm, i)
-            @constraint(pm.model,  vm_vio[i] >=  vm[i] - bus["vmax"])
-            @constraint(pm.model,  vm_vio[i] >= -vm[i] + bus["vmin"])
         end
     end
+    var(pm, pm.cnw)[:vm] = vm
+
 
     for i in ids(pm, :ref_buses)
         PowerModels.constraint_theta_ref(pm, i)
     end
 
-    p = Dict()
-    q = Dict()
-    for (i,branch) in ref(pm, :branch)
-        #PowerModels.constraint_ohms_yt_from(pm, i)
-        #PowerModels.constraint_ohms_yt_to(pm, i)
 
-        #branch = ref(pm, :branch, i)
-        f_bus_id = branch["f_bus"]
-        t_bus_id = branch["t_bus"]
-        f_idx = (i, f_bus_id, t_bus_id)
-        t_idx = (i, t_bus_id, f_bus_id)
-
-        f_bus = ref(pm, :bus, f_bus_id)
-        t_bus = ref(pm, :bus, t_bus_id)
-
-        g, b = PowerModels.calc_branch_y(branch)
-        tr, ti = PowerModels.calc_branch_t(branch)
-        g_fr = branch["g_fr"]
-        b_fr = branch["b_fr"]
-        g_to = branch["g_to"]
-        b_to = branch["b_to"]
-        tm = branch["tap"]
-
-
-        #p_fr  = var(pm, :p, f_idx)
-        #q_fr  = var(pm, :q, f_idx)
-        #p_to  = var(pm, :p, t_idx)
-        #q_to  = var(pm, :q, t_idx)
-        va_fr = var(pm, :va, f_bus_id)
-        va_to = var(pm, :va, t_bus_id)
-        vm_fr = vm[f_bus_id]
-        vm_to = vm[t_bus_id]
-
-        if branch["transformer"]
-            p[f_idx] = @NLexpression(pm.model,  (g/tm^2+g_fr)*vm_fr^2 + (-g*tr+b*ti)/tm^2*(vm_fr*vm_to*cos(va_fr-va_to)) + (-b*tr-g*ti)/tm^2*(vm_fr*vm_to*sin(va_fr-va_to)) )
-            q[f_idx] = @NLexpression(pm.model, -(b/tm^2+b_fr)*vm_fr^2 - (-b*tr-g*ti)/tm^2*(vm_fr*vm_to*cos(va_fr-va_to)) + (-g*tr+b*ti)/tm^2*(vm_fr*vm_to*sin(va_fr-va_to)) )
-        else
-            p[f_idx] = @NLexpression(pm.model,  (g+g_fr)/tm^2*vm_fr^2 + (-g*tr+b*ti)/tm^2*(vm_fr*vm_to*cos(va_fr-va_to)) + (-b*tr-g*ti)/tm^2*(vm_fr*vm_to*sin(va_fr-va_to)) )
-            q[f_idx] = @NLexpression(pm.model, -(b+b_fr)/tm^2*vm_fr^2 - (-b*tr-g*ti)/tm^2*(vm_fr*vm_to*cos(va_fr-va_to)) + (-g*tr+b*ti)/tm^2*(vm_fr*vm_to*sin(va_fr-va_to)) )
-        end
-        p[t_idx] = @NLexpression(pm.model,  (g+g_to)*vm_to^2 + (-g*tr-b*ti)/tm^2*(vm_to*vm_fr*cos(va_to-va_fr)) + (-b*tr+g*ti)/tm^2*(vm_to*vm_fr*sin(va_to-va_fr)) )
-        q[t_idx] = @NLexpression(pm.model, -(b+b_to)*vm_to^2 - (-b*tr+g*ti)/tm^2*(vm_to*vm_fr*cos(va_to-va_fr)) + (-g*tr-b*ti)/tm^2*(vm_to*vm_fr*sin(va_to-va_fr)) )
+    start_time = time()
+    for i in ids(pm, :branch)
+        expression_branch_power_yt_from_goc(pm, i)
+        expression_branch_power_yt_to(pm, i)
     end
+    #Memento.info(LOGGER, "flow expr time: $(time() - start_time)")
 
-
-    pg = Dict()
-    qg = Dict()
+    pg = Dict{Int,Any}()
+    qg = Dict{Int,Any}()
     for (i,gen) in ref(pm, :gen)
         if gen["pg_fixed"]
-            pg[i] = gen["pg"]
             @constraint(pm.model, var(pm, :pg, i) == gen["pg"])
+            pg[i] = gen["pg"]
         else
-            pg[i] = @NLexpression(pm.model, gen["pg_base"] + gen["alpha"]*delta)
             @constraint(pm.model, var(pm, :pg, i) == gen["pg_base"] + gen["alpha"]*delta)
+            pg[i] = @NLexpression(pm.model, gen["pg_base"] + gen["alpha"]*delta)
         end
 
         if gen["qg_fixed"]
-            qg[i] = gen["qg"]
             @constraint(pm.model, var(pm, :qg, i) == gen["qg"])
             @constraint(pm.model, qg_vio[i] == 0)
+            qg[i] = gen["qg"]
         else
+            @constraint(pm.model,  qg_vio[i] >=  var(pm, :qg, i) - gen["qmax"])
+            @constraint(pm.model,  qg_vio[i] >= -var(pm, :qg, i) + gen["qmin"])
             qg[i] = var(pm, :qg, i)
-            @constraint(pm.model,  qg_vio[i] >=  qg[i] - gen["qmax"])
-            @constraint(pm.model,  qg_vio[i] >= -qg[i] + gen["qmin"])
         end
     end
+    var(pm, pm.cnw)[:pg] = pg
+    var(pm, pm.cnw)[:qg] = qg
 
 
     for (i,bus) in ref(pm, :bus)
-        #PowerModels.constraint_power_balance(pm, i)
-
-        #bus = ref(pm, :bus, i)
-        bus_arcs = ref(pm, :bus_arcs, i)
-        bus_arcs_dc = ref(pm, :bus_arcs_dc, i)
-        bus_gens = ref(pm, :bus_gens, i)
-        bus_loads = ref(pm, :bus_loads, i)
-        bus_shunts_const = ref(pm, :bus_shunts_const, i)
-        bus_shunts_var = ref(pm, :bus_shunts_var, i)
-
-        bus_pd = Dict(k => ref(pm, :load, k, "pd") for k in bus_loads)
-        bus_qd = Dict(k => ref(pm, :load, k, "qd") for k in bus_loads)
-
-        bus_gs_const = Dict(k => ref(pm, :shunt, k, "gs") for k in bus_shunts_const)
-        bus_bs_const = Dict(k => ref(pm, :shunt, k, "bs") for k in bus_shunts_const)
-
-
-        #p = var(pm, :p)
-        #q = var(pm, :q)
-        #pg = var(pm, :pg)
-        #qg = var(pm, :qg)
-
-        @NLconstraint(pm.model, sum(p[a] for a in bus_arcs) == sum(pg[g] for g in bus_gens) - sum(pd for pd in values(bus_pd)) - sum(gs for gs in values(bus_gs_const))*vm[i]^2)
-        @NLconstraint(pm.model, sum(q[a] for a in bus_arcs) == sum(qg[g] for g in bus_gens) - sum(qd for qd in values(bus_qd)) + sum(bs for bs in values(bus_bs_const))*vm[i]^2 + sum(bs[s]*vm[i]^2 for s in bus_shunts_var))
+        constraint_power_balance_shunt_dispatch(pm, i)
     end
 
 end
